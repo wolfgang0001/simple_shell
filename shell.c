a8ffc89918672f2fe88ab019d59528fd30c815c7
kinecon12 <kinecon12@gmail.com>
2023-11-17 17:10:43 -0800

    update shell project

--- shell.c
+++ shell.c
@@ -1,28 +1,17 @@
-#include "simple.h"
+#include "shell.h"
 
 /**
- * main - This is the entry point for the program.
- * Author: kingsley
- * @pa: Number of Argument passed to the executable file. In the case
- * this variable will not be used.
- * @name: The name of the program.
- * Return: Always 0.
- */
-int main(__attribute__((unused)) int pa, char **name)
+ * main - func with infinite loop
+ * @ac: No use
+ * @av: No use
+ * Return: loop.
+ **/
+int main(int ac, char **av)
 {
-	char *line;
-	size_t size;
-	int command_counter;
+	(void)av;
+	(void)ac;
 
-	command_counter = 0;
-	signal(SIGINT, SIG_IGN);
-	do {
-		command_counter++;
-		line = NULL;
-		size = 0;
-		parse_line(line, size, command_counter, name);
-
-	} while (1);
-
+	signal(SIGINT, controlC);
+	prompt();
 	return (0);
 }
